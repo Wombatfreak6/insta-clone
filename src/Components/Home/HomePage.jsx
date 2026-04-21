3499088a768d84fe801726fd705fb6a88e476174
Jatin <jatinnagwani07@gmail.com>
2026-04-21 10:53:39 +0530

    Added StoriesBar component with gradient rings

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,10 +1,14 @@
 import Feed from "./Feed";
+import StoriesBar from "./StoriesBar";
 import "./HomePage.css";
 
 export default function HomePage() {
   return (
     <div className="home-page">
-      <Feed />
+      <div className="home-center">
+        <StoriesBar />
+        <Feed />
+      </div>
     </div>
   );
 }
