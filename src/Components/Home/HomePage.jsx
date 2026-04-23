e73f76e67890193686d3eed2fa6d0a86d7a85f83
Ashish2106B <ashish21bisht@gmail.com>
2026-04-23 12:33:42 +0530

    Errors in Suggestion so deleted the component

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,6 +1,5 @@
 import Feed from "./Feed";
 import StoriesBar from "./StoriesBar";
-import Suggestion from "../Suggestion/Suggestion";
 import "./HomePage.css";
 
 export default function HomePage() {
@@ -9,9 +8,6 @@ export default function HomePage() {
       <div className="home-center">
         <StoriesBar />
         <Feed />
-      </div>
-      <div className="home-right">
-        <Suggestion />
       </div>
     </div>
   );
